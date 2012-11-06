41db7e83dccd05e3b35cc866997a19f0318f82a0
Derek Dohler <derek@transparency.ge>
2012-11-06 12:57:11 +0400

    Breadth-first search for development.

--- registry/settings.py
+++ registry/settings.py
@@ -11,6 +11,11 @@ BOT_NAME = 'registry'
 SPIDER_MODULES = ['registry.spiders']
 NEWSPIDER_MODULE = 'registry.spiders'
 
+#DEBUG ONLY
+DEPTH_PRIORITY = 1
+SCHEDULER_DISK_QUEUE = 'scrapy.squeue.PickleFifoDiskQueue'
+SCHEDULER_MEMORY_QUEUE = 'scrapy.squeue.FifoMemoryQueue'
+
 #COOKIES_DEBUG = True
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'registry (+http://www.yourdomain.com)'
