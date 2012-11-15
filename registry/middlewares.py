eb957e58894dd4c2df397a25c19a7b2838ea1298
Derek Dohler <derek@transparency.ge>
2012-11-15 11:22:15 +0400

    Added PDF to XML conversion to middleware

---
+++ registry/middlewares.py
@@ -0,0 +1,44 @@
+import uuid, os
+
+from registry.settings import PDFTOHTML_TEMP_DIR as tmp
+# Custom middleware that downloads a PDF file, converts
+# it to HTML / XML using either Python or an external
+# program, and then provides the result as if it were a
+# request.
+class PdfToHtmlMiddleware(object):
+    
+    def process_response(self, request, response, spider):
+        # Relies on the server setting the content-type header 
+        # correctly.
+        if response.headers['content-type'] != "application/pdf":
+            return response
+        
+        # If it's a PDF
+        else:
+            # Generate a unique file name
+            fname = str(uuid.uuid1())
+            
+            # Open file for writing
+            # Dump response to file
+            with open(tmp+'/'+fname+'.pdf', 'wb') as f:
+                f.write(response.body)
+
+            # Execute PDF-to-HTML (however)
+            # pdftohtml from the poppler suite, executed with the
+            # following options:
+            # -q: Don't print messages or errors
+            # -xml: Output xml, not HTML
+            # -s: Generate a single document with all PDF pages
+            # -i: ignore images
+            # -enc: Encoding
+            os.system('pdftohtml -q -xml -s -i -enc UTF-8 {} {}'.format(tmp+'/'+fname+'.pdf',tmp+'/'+fname))
+            # Read output
+            with open(tmp+'/'+fname+'.xml', 'rb') as fin:
+                new_body = fin.read()
+            
+            # Clean up after ourselves
+            os.remove(tmp+'/'+fname+'.pdf')
+            os.remove(tmp+'/'+fname+'.xml')
+
+            # Construct new response
+            return response.replace(body=new_body)
